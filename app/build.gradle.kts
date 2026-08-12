03da0f64dc55de979b75f6efcbf579a5ec6c0aaa
poberij-crypto <poberij@gmail.com>
2026-08-12 10:32:14 +0400

    Use Lifecycle 2.10.0 for SDK 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.11.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.11.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
