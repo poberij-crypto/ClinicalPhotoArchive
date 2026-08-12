158d0e1c890923a0633baa7911aad681da46ae55
poberij-crypto <poberij@gmail.com>
2026-08-12 10:27:22 +0400

    Use stable Android SDK 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ ksp {
 
 android {
     namespace = "com.clinicalphotoarchive"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.clinicalphotoarchive"
         minSdk = 28
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
 
