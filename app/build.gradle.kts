7b8a00433f5014fd1e58eb9ed4b361225d79bd51
poberij-crypto <poberij@gmail.com>
2026-09-19 15:34:28 +0400

    Bump version to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.clinicalphotoarchive"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
 
         vectorDrawables {
             useSupportLibrary = true
